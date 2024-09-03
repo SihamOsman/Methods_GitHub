c6980c6156afc3ffa95d743340c48ca4590dbeb6
Siham Osman (PRIME 8 LLC) <v-osmansiham@microsoft.com>
2024-09-03 13:29:24 -0700

    commit

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int totalCalulatedRisks= totalCalculatedRisk();
+//        System.out.println("The TotalCaluclatedRisk is: " + totalCalulatedRisks);
+    }
+
+    public static int calculateCrimeRisk() {
+        int Crime = 77;
+       // System.out.println(Crime);
+        return Crime;
+    }
+
+    public static int calculateFloodRisk() {
+        int Flood = 87;
+        return Flood;
+    }
+    public static int calculateEarhquakeRisk() {
+        int EarthQuick = 90;
+        return EarthQuick;
+    }
+//    public static int totalCalculatedRisk() {
+//
+//       int result1 =calculateCrimeRisk();
+//       int result2 =calculateEarhquakeRisk();
+//       int result3=calculateFloodRisk();
+//       return result1+result2+result3;
+//        }
+    }
+
